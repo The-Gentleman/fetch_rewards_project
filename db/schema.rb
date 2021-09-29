1827820c68fa57c54b1f2f43cc2c292031922eb4
The-Gentleman <zraja4402@gmail.com>
2021-09-29 02:16:36 -0400

    added other stuff

---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_29_060957) do
+
+  create_table "payers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "points", force: :cascade do |t|
+    t.integer "point_count"
+    t.integer "payer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["payer_id"], name: "index_points_on_payer_id"
+  end
+
+  create_table "transaction_dates", force: :cascade do |t|
+    t.date "date_of_transaction"
+    t.integer "point_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["point_id"], name: "index_transaction_dates_on_point_id"
+  end
+
+  add_foreign_key "points", "payers"
+  add_foreign_key "transaction_dates", "points"
+end
---
+++ test/controllers/transaction_dates_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TransactionDatesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
