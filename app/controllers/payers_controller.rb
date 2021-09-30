5ba440e71cb7885d2d5b467403e62e6926ea7068
The-Gentleman <zraja4402@gmail.com>
2021-09-30 16:18:24 -0400

    added points instace variable to show current points

--- app/controllers/payers_controller.rb
+++ app/controllers/payers_controller.rb
@@ -5,6 +5,7 @@ class PayersController < ApplicationController
 
     def show 
         @payer = Payer.find_by(id: params[:id])
+        @point = Point.find{|point| point.payer_id == params[:id].to_i}
     end 
    
 end
