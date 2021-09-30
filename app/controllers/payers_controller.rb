6efa057d7dee9d60800d474156c8834e98cbf4b7
The-Gentleman <zraja4402@gmail.com>
2021-09-30 16:09:41 -0400

    added index and show actions for payers controller

--- app/controllers/payers_controller.rb
+++ app/controllers/payers_controller.rb
@@ -1,4 +1,10 @@
 class PayersController < ApplicationController
+    def index
+        @payers = Payer.all
+    end 
 
+    def show 
+        @payer = Payer.find_by(id: params[:id])
+    end 
    
 end
