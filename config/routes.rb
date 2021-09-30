1046163ad7f7c9c0b2b5b28b5d85c08e77deb144
The-Gentleman <zraja4402@gmail.com>
2021-09-30 13:45:28 -0400

    added routes to display balances

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  resources :transaction_dates
+  # Id like this to display balances
+  get '/payers/balances', to: 'payers#index', as: :balances
+
+
+  resources :payers, only: [:index, :show, :balances]
   resources :points
-  resources :payers
+  resources :transaction_dates
+
+
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
